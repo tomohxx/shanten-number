15afefd2a59634dd9f24e5b2225ee0bfa915511c
tomohxx <43703429+tomohxx@users.noreply.github.com>
2026-05-09 00:15:05 +0900

    perf: optimize dp loop ordering in mkind

--- src/mkind.cpp
+++ src/mkind.cpp
@@ -43,19 +43,14 @@ void dp(const Hand<N>& hand, const std::vector<Delta>& deltas, std::array<uint8_
   table[0][0][0][0][0] = 0;
 
   for (int n = 0; n < N; ++n) {
-    for (int a = 0; a <= 4; ++a) {
-      for (int b = 0; b <= 4; ++b) {
-        for (int h = 0; h <= 1; ++h) {
-          for (int m = 0; m <= 4; ++m) {
-            auto& tmp = table[n][a][b][h][m];
+    for (const auto& delta : deltas) {
+      for (int a = 0; a <= 4 - delta.a; ++a) {
+        for (int b = 0; b <= std::min(4 - delta.b, a); ++b) {
+          for (int h = 0; h <= 1 - delta.h; ++h) {
+            for (int m = 0; m <= 4 - delta.m; ++m) {
+              const auto& tmp = table[n][a][b][h][m];
 
-            if (tmp == MAX_SHT) continue;
-
-            for (const auto& delta : deltas) {
-              if (a + delta.a > 4 ||
-                  b + delta.b > 4 ||
-                  h + delta.h > 1 ||
-                  m + delta.m > 4) continue;
+              if (tmp == MAX_SHT) continue;
 
               chmin(table[n + 1][b + delta.b][delta.c][h + delta.h][m + delta.m],
                     tmp + std::max(a + delta.a - hand[n], 0));
